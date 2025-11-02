190c1f97829de0ce7d63c870dd39e6cad15ed91b
Tinilinsk <120578910+Tinilinsk@users.noreply.github.com>
2025-11-02 16:03:10 +0100

    Added product class

---
+++ CRUDproduct/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDproduct.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
